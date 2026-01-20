500a52a21bab586ff9d28032d4af72d1c5b6d5ba
naorb12 <naorb12@gmail.com>
2026-01-20 12:37:45 +0200

    feat: implement generateDashboard controller and update dashboard routes for user authentication

---
+++ server/src/controllers/dashboard.controller.js
@@ -0,0 +1,18 @@
+import { buildDashboard } from "../services/dashboard/dashboard.service.js";
+
+export async function generateDashboard(req, res, next) {
+  try {
+    const userId = req.user.id;
+    if (!userId) {
+      return res.status(400).json({ error: "No user id" });
+    }
+
+    const dashboard = await buildDashboard(userId);
+    res.status(201).json(dashboard);
+  } catch (err) {
+    if (err.message === "Preferences don't exist") {
+      return res.status(400).json({ error: err.message });
+    }
+    res.status(500).json({ error: err });
+  }
+}
--- server/src/routes/dashboard.routes.js
+++ server/src/routes/dashboard.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import { auth } from "../middleware/auth.js";
+import { generateDashboard } from "../controllers/dashboard.controller.js";
 
 const router = new Router();
 
-// router.post("/", gestDashboard);
+router.post("/", auth, generateDashboard);
 export default router;
